9dd3f1b82b783282334105ce08fdb25609909dd9
shredder-vrn <p_shcherbatov@mail.ru>
2024-11-16 10:56:51 +0300

    commit #5 (vectors: interface)

--- src/wings/math/vectors/Vector.java
+++ src/wings/math/vectors/Vector.java
@@ -1,4 +1,41 @@
 package wings.math.vectors;
 
+/**
+ * Интерфейс Vector, определяющий основные операции для векторов.
+ */
 public interface Vector {
+    /**
+     * Сложение с другим вектором.
+     */
+    Vector add(Vector v2);
+
+    /**
+     * Вычитание другого вектора.
+     */
+    Vector subtract(Vector v2);
+
+    /**
+     * Умножение вектора на скаляр.
+     */
+    Vector scale(double scalar);
+
+    /**
+     * Деление вектора на скаляр.
+     */
+    Vector divide(double scalar);
+
+    /**
+     * Вычисление длины вектора.
+     */
+    double length();
+
+    /**
+     * Нормализация вектора (приведение длины к 1).
+     */
+    Vector normalize();
+
+    /**
+     * Скалярное произведение с другим вектором.
+     */
+    double dotProduct(Vector v2);
 }
--- src/wings/math/vectors/Vector3.java
+++ src/wings/math/vectors/Vector3.java
@@ -1,4 +1,38 @@
 package wings.math.vectors;
 
-public class Vector3 {
+public class Vector3 implements Vector{
+    @Override
+    public Vector add(Vector v2) {
+        return null;
+    }
+
+    @Override
+    public Vector subtract(Vector v2) {
+        return null;
+    }
+
+    @Override
+    public Vector scale(double scalar) {
+        return null;
+    }
+
+    @Override
+    public Vector divide(double scalar) {
+        return null;
+    }
+
+    @Override
+    public double length() {
+        return 0;
+    }
+
+    @Override
+    public Vector normalize() {
+        return null;
+    }
+
+    @Override
+    public double dotProduct(Vector v2) {
+        return 0;
+    }
 }
